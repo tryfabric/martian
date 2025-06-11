104eeff81e1be214c15062fec9c6366740390ce5
Akos Orban <akos.orban@pronos.eu>
2025-06-11 22:24:13 +0200

    feat: enhance paragraph parsing to support hard line breaks
    This update modifies the paragraph parsing logic to correctly split paragraphs on hard line breaks, improving the handling of markdown input. Additionally, new tests have been added to ensure the functionality works as expected.

--- test/integration.spec.ts
+++ test/integration.spec.ts
@@ -218,6 +218,30 @@ const hello = "hello";
 
       expect(actual).toStrictEqual(expected);
     });
+
+    it('should split paragraphs on hard line breaks', () => {
+      const text =
+        'You can _italicize_ or **bold** text.  \nThis is the second line of text.\nAnd this is in the same line';
+
+      const actual = markdownToBlocks(text);
+
+      const expected = [
+        notion.paragraph([
+          notion.richText('You can '),
+          notion.richText('italicize', {annotations: {italic: true}}),
+          notion.richText(' or '),
+          notion.richText('bold', {annotations: {bold: true}}),
+          notion.richText(' text.'),
+        ]),
+        notion.paragraph([
+          notion.richText(
+            'This is the second line of text.\nAnd this is in the same line',
+          ),
+        ]),
+      ];
+
+      expect(actual).toStrictEqual(expected);
+    });
   });
 
   describe('markdownToRichText', () => {
